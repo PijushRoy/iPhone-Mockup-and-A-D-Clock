5c5ec56d6a5b930e4345cf1b2fb96e29427eaa63
PijushRoy <pijushroy0198@gmail.com>
2021-09-11 02:02:19 +0600

    style update

--- app.js
+++ app.js
@@ -2,27 +2,23 @@ setInterval(() => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let second = date.getSeconds();
+    // let second = date.getSeconds();
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    second = second < 10 ? '0' + second : second;
-    var ampm = hours >= 12 ? 'PM' : 'AM';
+    // second = second < 10 ? '0' + second : second;
+    // var ampm = hours >= 12 ? 'PM' : 'AM';
 
     document.getElementById('hour').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
 
-    document.getElementById("digital-hr").innerHTML = hours
-    document.getElementById("digital-min").innerHTML = minutes
-    document.getElementById("digital-sec").innerHTML = second
-    document.getElementById("digital-ampm").innerHTML = ampm
-
 
     let today = date.getDay()
     let dayList = ['SunDay', 'MonDay', 'TuesDay', 'WednesDay', 'ThusDay', 'FriDay', 'SaturDay']
-    let currentDay = `ToDay is: ${dayList[today]}.`
+    let currentDay = `ToDay is ${dayList[today]}`
     document.getElementById("day").innerHTML = currentDay
+    console.log(currentDay)
 }, 500)
 
 
